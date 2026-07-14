da05c6c49f338e40a9125161eb4ba069a45498f1
yassine-ed <yassine.eddaadoua37@gmail.com>
2026-07-14 19:22:28 +0100

    feat: implement main logic and javascript functionality

---
+++ project-2/script.js
@@ -0,0 +1,27 @@
+// Variable 
+
+let openBtn = document.getElementById("open-btn");
+let modelContainer = document.getElementById("modal-container");
+let closeBtn =  document.getElementById("close-btn");
+
+// Event Listeners 
+
+openBtn.addEventListener("click", function(){
+
+    modelContainer.style.display = "block";
+
+});
+
+closeBtn.addEventListener("click", function(){
+    
+    modelContainer.style.display = "none";
+
+});
+
+window.addEventListener("click", function(e) {
+
+    if (e.target === modelContainer){
+        modelContainer.style.display = "none";
+    }
+
+});
